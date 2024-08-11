155f5cc5359c286e620e818582b68048a464cd55
Nguyen Long <longsky0912624119@gmail.com>
2024-08-12 01:29:32 +0700

    add form register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,79 @@
+import { Button, Form, Input } from "antd";
 
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+    const onFinish = (values) => { }
     return (
-        <div>Register Page</div>
+        <Form
+            form={form}
+            layout="vertical"
+
+            onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+
+        >
+
+            <div style={{
+                margin: "50px",
+
+            }}>
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone number"
+                    name="phone"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+
+
+
+                <div>
+                    <Button onClick={() => form.submit()} type="primary">Register</Button>
+                </div>
+            </div>
+        </Form>
+
     )
 }
 export default RegisterPage;
