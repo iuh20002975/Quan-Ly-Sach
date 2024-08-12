d663a3025bdb23231172cb7463d45146fcffc549
Nguyen Long <longsky0912624119@gmail.com>
2024-08-12 19:18:32 +0700

    complete logout and redirect home page

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,15 +1,17 @@
 
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 // import './header.css';
-import { Menu } from 'antd';
+import { Menu, message } from 'antd';
 import { UsergroupAddOutlined, HomeOutlined, ContainerOutlined, SettingOutlined, LoginOutlined, AliwangwangOutlined } from '@ant-design/icons';
 import { useContext, useState } from 'react';
 import { AuthContext } from '../context/auth.context';
+import { logoutAPI } from '../../services/api.service';
 const Header = () => {
 
     const [current, setCurrent] = useState('');
+    const navigate = useNavigate();
 
-    const { user } = useContext(AuthContext);
+    const { user, setUser } = useContext(AuthContext);
 
 
 
@@ -17,6 +19,28 @@ const Header = () => {
         console.log('click ', e);
         setCurrent(e.key);
     };
+
+
+    const handleLogout = async () => {
+        const res = await logoutAPI();
+        if (res.data) {
+            //clear data
+            localStorage.removeItem("access_token");
+            setUser({
+                email: "",
+                phone: "",
+                fullName: "",
+                role: "",
+                avatar: "",
+                id: ""
+            })
+            message.success("Logout thành công.");
+
+
+            //redirect to home
+            navigate("/");
+        }
+    }
 
     const items = [
         {
@@ -49,7 +73,7 @@ const Header = () => {
             icon: <AliwangwangOutlined />,
             children: [
                 {
-                    label: 'Đăng xuất',
+                    label: <span onClick={() => handleLogout()}>Đăng xuất</span>,
                     key: 'logout',
                 },
             ],
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -91,6 +91,14 @@ const getAccountAPI = () => {
 }
 
 
+const logoutAPI = () => {
+    const URL_BACKEND = "/api/v1/auth/logout";
+
+    return axios.post(URL_BACKEND);
+
+}
+
+
 export {
-    createUserAPI, updateUserAPI, fetchAllUserAPI, deleteUserAPI, handleUploadFile, updateUserAvatarAPI, registerUserAPI, loginAPI, getAccountAPI
+    createUserAPI, updateUserAPI, fetchAllUserAPI, deleteUserAPI, handleUploadFile, updateUserAvatarAPI, registerUserAPI, loginAPI, getAccountAPI, logoutAPI
 }
